0aae35b2796a31953345516d64bebf3f0cfd21fb
Melinda Chang <melindachang.hy@gmail.com>
2022-08-06 10:17:29 -0700

    merge

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-<<<<<<< HEAD
-    let time = 300;
-=======
 
     let time = 500;
     let price = 10.50;
     let counter = 2;
->>>>>>> f09b44ba93590380d17d259ed75e90850e498a27
     let timer = setInterval(() => {
         document.querySelector('#price').innerHTML = "The Current Price is : $" + price;
         time--;
