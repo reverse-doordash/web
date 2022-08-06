32d6c854f185f5bddc474972aede75987b9858ac
val-hir <jinpark11251125@gmail.com>
2022-08-06 08:19:52 -0700

    confirmation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let time = 300;
-    let timer = setInterval(() => {
-        let minutes = Math.trunc(time / 60)
-        let seconds = (time % 60)
-        if(seconds > 9){
-            document.querySelector('#timerdetail').innerHTML = minutes + " : " + seconds;
-        }
-        else if(seconds < 10){
-            document.querySelector('#timerdetail').innerHTML = minutes + " : 0" + seconds;
-        }
-        time--;
-        if (time < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
+    
+    function caughtlackin() {
+        let time = 5;
+        let timer = setInterval(() => {
+
+
+
+            document.querySelector('#caught').innerHTML = time;
+            time--;
+            if (time < 0) {
+                clearInterval(timer);
+                window.locate.href = '/views/confirm.html';
+            }
+        }, 1000);
+    }
 
 })
