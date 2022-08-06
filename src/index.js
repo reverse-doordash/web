afa0616395fb664c4120a54188e682bee0970cde
val-hir <jinpark11251125@gmail.com>
2022-08-06 08:36:25 -0700

    price double and fake fetch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    function caughtlackin() {
-        let time = 5;
-        let timer = setInterval(() => {
 
-
-
-            document.querySelector('#caught').innerHTML = time;
-            time--;
-            if (time < 0) {
-                clearInterval(timer);
-                window.locate.href = '/views/confirm.html';
-            }
-        }, 1000);
-    }
+    let time = 500;
+    let price = 10.50;
+    let counter = 2;
+    let timer = setInterval(() => {
+        document.querySelector('#price').innerHTML = "The Current Price is : $" + price;
+        time--;
+        if (time < 0) {
+            price *= counter;
+            counter++;d
+            time = 300;
+            clearInterval(timer);
+        }
+    }, 1000);
 
 })
